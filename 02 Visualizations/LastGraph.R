9bef024c5c19094cb8db87c9665e6c752fa887ad
amurga1 <adrimurga@Adrianas-MacBook-Air.local>
2015-09-13 17:33:01 -0500

    Le cambie los signos de + al final del codigo

--- 02 Visualizations/LastGraph.R
+++ 02 Visualizations/LastGraph.R
@@ -1,21 +1,21 @@
-
 require("jsonlite")
 require("RCurl")
+
+#Graph 1
 # Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_ams6624', PASS='orcl_ams6624', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs347_professor', PASS='orcl_professor', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 df
 summary(df)
 head(df)
-
 require(extrafont)
 ggplot(data = df, 
-      mapping = aes(x = factor(PCLASS), color =SEX, fill =SEX))
-+ coord_cartesian() 
-+ scale_x_discrete() 
-+ scale_y_continuous()
-+ labs(title='Titanic') 
-+ labs(x=paste("Pclass"), y=paste("count"))
-+ layer(geom = "bar",
+      mapping = aes(x = factor(PCLASS), color =SEX, fill =SEX)) +
+ coord_cartesian() +
+ scale_x_discrete() +
+ scale_y_continuous() +
+ labs(title='Titanic') +
+ labs(x=paste("Pclass"), y=paste("count")) +
+ layer(geom = "bar",
           position = "dodge",
           stat = "bin",
           data = df, 
