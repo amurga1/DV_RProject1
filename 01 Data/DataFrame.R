070c984cbb8edcf0f3b92c511a24ee0e598655db
Aura Maria Salazar <auramariasalazar94@gmail.com>
2015-09-15 10:27:04 -0500

    Changes to RMD file. Successful!

--- 01 Data/DataFrame.R
+++ 01 Data/DataFrame.R
@@ -1,9 +1,6 @@
 require("jsonlite")
 require("RCurl")
-@
-<< chunk1, include=FALSE>>=
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs347_professor', PASS='orcl_professor', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-@
-df
-summary(df)
+#df
+#summary(df)
 head(df)
